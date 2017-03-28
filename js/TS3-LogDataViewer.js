Drumsticks1/TS3-LogDataViewer

commit 5d6781b8237b7b591e56c59cf09bfa8caf9c3009

Replaced in-/equality checks with strict in-/equality checks in the javascript code (== --> ===, != --> !==)

js/TS3-LogDataViewer.js
@@ -46,7 +46,7 @@ function buildJSON(clearBuffer) {
   /* Requests are only sent if the time span between the last two requests is bigger than timeBetweenRequests and
    there is no request in progress. Invalid requests are only sent when the remote timeBetweenRequests is unknown
    or has increased since the last request */
-  if (!buildRequestInProgress && (lastBuildCallTime == null || timeBetweenBuilds == null || Date.now().valueOf() - lastBuildCallTime > timeBetweenBuilds)) {
+  if (!buildRequestInProgress && (lastBuildCallTime === null || timeBetweenBuilds === null || Date.now().valueOf() - lastBuildCallTime > timeBetweenBuilds)) {
     buildRequestInProgress = true;
     lastBuildCallTime = Date.now().valueOf();
 
@@ -90,7 +90,7 @@ function buildJSON(clearBuffer) {
  * @param {object} response response object of a request.
  */
 function updateTimeBetweenBuilds(response) {
-  if (timeBetweenBuilds != response.timeBetweenRequests)
+  if (timeBetweenBuilds !== response.timeBetweenRequests)
     timeBetweenBuilds = response.timeBetweenRequests;
 }
 
@@ -107,9 +107,9 @@ function addCallout(message, calloutClass, duration) {
   var calloutsWithTheSameClass = document.getElementsByClassName(calloutClass);
 
   // Prevents duplicate callouts
-  if (calloutsWithTheSameClass.length != 0) {
+  if (calloutsWithTheSameClass.length !== 0) {
     // Updates the message of the nextRequestCallout if one is already existing.
-    if (calloutClass.indexOf("nextRequestCallout") != -1)
+    if (calloutClass.indexOf("nextRequestCallout") !== -1)
       calloutsWithTheSameClass[0].innerText = message;
 
     return;
@@ -129,7 +129,7 @@ function addCallout(message, calloutClass, duration) {
   callout.appendChild(calloutCloseButton);
   document.getElementById("calloutDiv").appendChild(callout);
 
-  if (duration != undefined) {
+  if (duration !== undefined) {
     setTimeout(function () {
       $(callout).fadeOut(function () {
         callout.parentNode.removeChild(callout);
@@ -146,7 +146,7 @@ function addCallout(message, calloutClass, duration) {
  */
 function expandOrCollapseList(list, ID) {
   var column, collapsedCellCount;
-  if (list == "IPs") {
+  if (list === "IPs") {
     column = 3;
     collapsedCellCount = 1;
   } else
@@ -154,7 +154,7 @@ function expandOrCollapseList(list, ID) {
 
   var currentDiv = document.getElementById(list + "_" + ID + "_1");
 
-  if (currentDiv === null || currentDiv.style.display == "none")
+  if (currentDiv === null || currentDiv.style.display === "none")
     expandList(list, ID, column, collapsedCellCount);
   else
     collapseList(list, ID, column, collapsedCellCount);
@@ -183,12 +183,12 @@ function expandList(list, ID, column, collapsedCellCount) {
       var newDiv = document.createElement("div");
       newDiv.id = currentDivString;
 
-      if (collapsedCellCount == 1)
+      if (collapsedCellCount === 1)
         newDiv.innerHTML = listContent[j];
       else
         newDiv.innerHTML = UTCDateStringToLocaltimeString(listContent[j]);
 
-      if (column == 3)
+      if (column === 3)
         x.appendChild(newDiv);
       else
         x.insertBefore(newDiv, x.lastChild);
@@ -214,7 +214,7 @@ function collapseList(list, ID, column, collapsedCellCount) {
   if (document.getElementById(String(ID)) !== null) {
     var hideLength = document.getElementById(String(ID)).childNodes[column].childNodes.length;
 
-    if (column == 3)
+    if (column === 3)
       hideLength++;
 
     for (var j = 1; j < hideLength - 2; j++) {
@@ -229,10 +229,10 @@ function collapseList(list, ID, column, collapsedCellCount) {
 function collapseAll() {
   var rows = document.getElementById("clientTable").lastChild.childNodes;
   for (var j = 0; j < rows.length; j++) {
-    if (rows[j].childNodes[2].getAttribute("expanded") == "true")
+    if (rows[j].childNodes[2].getAttribute("expanded") === "true")
       collapseList("Connections", Number(rows[j].getAttribute("id")), 2, 2);
 
-    if (rows[j].childNodes[3].getAttribute("expanded") == "true")
+    if (rows[j].childNodes[3].getAttribute("expanded") === "true")
       collapseList("IPs", Number(rows[j].getAttribute("id")), 3, 1);
   }
 }
@@ -255,8 +255,8 @@ function addCustomParsers() {
   $.tablesorter.addParser({
     id: "connections",
     format: function (s, table, cell) {
-      if (localStorage.getItem("connectionsSortType") == "1") {
-        if (cell.firstChild.localName == "button")
+      if (localStorage.getItem("connectionsSortType") === "1") {
+        if (cell.firstChild.localName === "button")
           return cell.childNodes[1].innerHTML;
         else
           return cell.firstChild.innerHTML;
@@ -269,7 +269,7 @@ function addCustomParsers() {
   $.tablesorter.addParser({
     id: "ips",
     format: function (s, table, cell) {
-      if (cell.firstChild.localName == "button")
+      if (cell.firstChild.localName === "button")
         return cell.childNodes[1].innerHTML;
       else
         return cell.firstChild.innerHTML;
@@ -308,12 +308,12 @@ function switchBetweenIDAndUID() {
     var currentRowCells = document.getElementById(rowId).childNodes;
 
     if (uidState) {
-      if (currentRowCells[3].innerHTML != "Unknown")
+      if (currentRowCells[3].innerHTML !== "Unknown")
         bannedByIDOrUID = Ban[banId].bannedByID;
       else
         bannedByIDOrUID = "Unknown";
     } else {
-      if (Ban[banId].bannedByUID.length != 0)
+      if (Ban[banId].bannedByUID.length !== 0)
         bannedByIDOrUID = Ban[banId].bannedByUID;
       else
         bannedByIDOrUID = "No UID";
@@ -356,7 +356,7 @@ function addTableCheckboxListener(table) {
     if (this.checked) {
       localStorage.setItem(table, "1");
 
-      if (sessionStorage.getItem(table + "-built") == "0")
+      if (sessionStorage.getItem(table + "-built") === "0")
         buildTableWithAlertCheckAndLocalStorage(table);
 
     } else {
@@ -526,11 +526,11 @@ function buildClientTable() {
 
   if (localStorage.getItem("connectionsSortType") === null)
     localStorage.setItem("connectionsSortType", "1");
-  else if (localStorage.getItem("connectionsSortType") == "0")
+  else if (localStorage.getItem("connectionsSortType") === "0")
     connectionsSortTypeButton.innerHTML = sortStrings[0];
 
   addOnClickEvent(connectionsSortTypeButton, function () {
-    if (localStorage.getItem("connectionsSortType") == "1") {
+    if (localStorage.getItem("connectionsSortType") === "1") {
       connectionsSortTypeButton.innerHTML = sortStrings[0];
       localStorage.setItem("connectionsSortType", "0");
     } else {
@@ -604,7 +604,7 @@ function buildClientTable() {
   var clientBody = document.createElement("tbody");
   for (var i = 0; i < Client.length; i++) {
     var ID = Number(Client[i].clientId);
-    if (Client[ID].Nicknames.length != 0) {
+    if (Client[ID].Nicknames.length !== 0) {
       var Nicknames = Client[ID].Nicknames,
         Connections = Client[ID].Connections,
         IPs = Client[ID].IPs,
@@ -734,7 +734,7 @@ function buildClientTable() {
           var connections = json.ClientList[Number(data.$cells[0].innerHTML)].Connections;
 
           for (i = 0; i < connections.length; i++) {
-            if (connections[i].indexOf(data.filter) != -1)
+            if (connections[i].indexOf(data.filter) !== -1)
               return true;
           }
 
@@ -744,7 +744,7 @@ function buildClientTable() {
           var ips = json.ClientList[Number(data.$cells[0].innerHTML)].IPs;
 
           for (i = 0; i < ips.length; i++) {
-            if (ips[i].indexOf(data.filter) != -1)
+            if (ips[i].indexOf(data.filter) !== -1)
               return true;
           }
 
@@ -834,12 +834,12 @@ function buildBanTable() {
       BanReason,
       BanTime = Ban[i].banTime;
 
-    if (BannedIP != "Unknown")
+    if (BannedIP !== "Unknown")
       BannedByID = Ban[i].bannedByID;
     else
       BannedByID = "Unknown";
 
-    if (Ban[i].banReason.length != 0)
+    if (Ban[i].banReason.length !== 0)
       BanReason = Ban[i].banReason;
     else
       BanReason = "No Reason given";
@@ -969,7 +969,7 @@ function buildKickTable() {
       KickedByUID = Kick[i].kickedByUID,
       KickReason;
 
-    if (Kick[i].kickReason.length != 0)
+    if (Kick[i].kickReason.length !== 0)
       KickReason = Kick[i].kickReason;
     else KickReason = "No Reason given";
 
@@ -1291,7 +1291,7 @@ function buildUploadTable() {
  * @param {string} table - name of the table (e.g. "clientTable").
  */
 function buildTableWithAlertCheckAndLocalStorage(table) {
-  if (document.getElementById(table) != null) {
+  if (document.getElementById(table) !== null) {
     $(document.getElementById(table)).trigger("destroy");
     $(document.getElementById("ts3-" + table)).empty();
   }
@@ -1299,7 +1299,7 @@ function buildTableWithAlertCheckAndLocalStorage(table) {
   if (localStorage.getItem(table + "SortOrder") === null)
     localStorage.setItem(table + "SortOrder", "[]");
 
-  if (localStorage.getItem(table) != "0") {
+  if (localStorage.getItem(table) !== "0") {
     sessionStorage.setItem(table + "-built", "1");
 
     var leadingCapitalLetterTable = table.charAt(0).toUpperCase() + table.substring(1);
@@ -1373,10 +1373,10 @@ function buildTables() {
 
       // Ban table UID state action.
       var uidState = localStorage.getItem("uidState");
-      if (document.getElementById("banTable") != null && (uidState == null || uidState == "1")) {
+      if (document.getElementById("banTable") !== null && (uidState === null || uidState === "1")) {
         localStorage.setItem("uidState", "0");
 
-        if (uidState == "1")
+        if (uidState === "1")
           switchBetweenIDAndUID();
       }
 
@@ -1392,7 +1392,7 @@ function buildTables() {
         document.getElementById("creationTimestamp_moment").innerHTML = moment(creationTime.UTC + " +0000", "DD.MM.YYYY HH:mm:ss Z").fromNow();
       }, 1000);
 
-      if (!document.getElementById("clientTable") || document.getElementById("ts3-clientTable").style.display == "none") {
+      if (!document.getElementById("clientTable") || document.getElementById("ts3-clientTable").style.display === "none") {
         var Client = json.ClientList;
         for (var j = 0; j < Client.length; j++) {
           if (Client[j].ConnectedState)

nodejs/Channel.js
@@ -79,7 +79,7 @@ module.exports = {
    */
   getChannelByChannelId: function (array, channelId) {
     for (var i = 0; i < array.length; i++) {
-      if (array[i].channelId == channelId)
+      if (array[i].channelId === channelId)
         return array[i];
     }
     return null;

nodejs/Client.js
@@ -82,7 +82,7 @@ Client.prototype.updateClientId = function (clientId) {
  */
 Client.prototype.addNickname = function (Nickname) {
   for (var i = 0; i < this.Nicknames.length; i++) {
-    if (this.Nicknames[i] == Nickname) {
+    if (this.Nicknames[i] === Nickname) {
       this.Nicknames.splice(i, 1);
     }
   }
@@ -103,7 +103,7 @@ Client.prototype.addConnection = function (Connection) {
  */
 Client.prototype.addIP = function (IP) {
   for (var i = 0; i < this.IPs.length; i++) {
-    if (this.IPs[i] == IP) {
+    if (this.IPs[i] === IP) {
       this.IPs.splice(i, 1);
     }
   }
@@ -159,7 +159,7 @@ Client.prototype.addServerGroup = function (ServerGroupID, ServerGroupAssignment
 Client.prototype.removeServerGroupByID = function (ServerGroupID) {
   var done = false;
   for (var i = 0; i < this.ServerGroupIDs.length && !done; i++) {
-    if (this.getServerGroupIDByID(i) == ServerGroupID) {
+    if (this.getServerGroupIDByID(i) === ServerGroupID) {
       this.ServerGroupIDs.splice(i, 1);
       this.ServerGroupAssignmentDateTimes.splice(i, 1);
       done = true;
@@ -169,16 +169,16 @@ Client.prototype.removeServerGroupByID = function (ServerGroupID) {
 
 /**
  * Returns the count of the ServerGroupIDs.
- * @returns {Number}
+ * @returns {number}
  */
 Client.prototype.getServerGroupIDCount = function () {
-  return this.ServerGroupIDs.length;
+  return Number(this.ServerGroupIDs.length);
 };
 
 /**
  * Returns the ServerGroupID in the ServerGroupIDsID slot.
  * @param {number} ServerGroupIDsID
- * @returns {String}
+ * @returns {number}
  */
 Client.prototype.getServerGroupIDByID = function (ServerGroupIDsID) {
   return this.ServerGroupIDs[ServerGroupIDsID];

nodejs/Parser.js
@@ -79,7 +79,7 @@ var match = {
  */
 String.prototype.indexOfEndPlusOne = function (searchString) {
   var indexOf = this.indexOf(searchString);
-  return indexOf == -1 ? -1 : indexOf + searchString.length;
+  return indexOf === -1 ? -1 : indexOf + searchString.length;
 };
 
 /**
@@ -98,7 +98,7 @@ module.exports = {
       if (!Logs[i].parsed && !Logs[i].ignored) {
         this.parseLogData(
           fs.readFileSync(globalVariables.TS3LogDirectory + Logs[i].logName, "utf8"),
-          i + 1 == Logs.length);
+          i + 1 === Logs.length);
 
         Logs[i].parsed = true;
       }
@@ -134,9 +134,9 @@ module.exports = {
       "|INFO    |VirtualServerBase|" + globalVariables.virtualServer + "  |"
     ];
 
-    if (logData.indexOf(match.VirtualServer[0] + 'listening on') != -1)
+    if (logData.indexOf(match.VirtualServer[0] + 'listening on') !== -1)
       return 0;
-    else if (logData.indexOf(match.VirtualServer[1] + 'listening on') != -1)
+    else if (logData.indexOf(match.VirtualServer[1] + 'listening on') !== -1)
       return 1;
 
     return -1;
@@ -155,7 +155,7 @@ module.exports = {
 
     // Todo: Add debug message.
     // Skip parsing if the log pattern is unknown.
-    if (logPattern == -1)
+    if (logPattern === -1)
       return;
 
     while (logData.length > 0) {
@@ -166,21 +166,21 @@ module.exports = {
         beginOfParsingBlock = currentLine.indexOfEndPlusOne(match.VirtualServer[logPattern]),
         checkIfUpload = false;
 
-      if (beginOfParsingBlock == -1) {
+      if (beginOfParsingBlock === -1) {
         lineSeverType = 1;
         beginOfParsingBlock = currentLine.indexOfEndPlusOne(match.VirtualServerBase[logPattern])
       }
 
       // Skip parsing for lines that aren't relevant.
-      if (beginOfParsingBlock == -1)
+      if (beginOfParsingBlock === -1)
         continue;
 
       switch (lineSeverType) {
         // VirtualSever
         case 0:
           // Client assignments to and client removals from a server group
-          if (currentLine.indexOf(match.serverGroupAssignment) != -1 || currentLine.indexOf(match.serverGroupRemoval) != -1) {
-            if (currentLine.indexOf(match.serverGroupAssignment) != -1)
+          if (currentLine.indexOf(match.serverGroupAssignment) !== -1 || currentLine.indexOf(match.serverGroupRemoval) !== -1) {
+            if (currentLine.indexOf(match.serverGroupAssignment) !== -1)
               res = parsers.serverGroup.parseServerGroupAssignment(currentLine);
             else
               res = parsers.serverGroup.parseServerGroupRemoval(currentLine);
@@ -196,7 +196,7 @@ module.exports = {
 
             Client.fillArrayWithDummyClients(ClientList, clientId);
 
-            if (currentLine.indexOf(match.serverGroupAssignment) != -1) {
+            if (currentLine.indexOf(match.serverGroupAssignment) !== -1) {
               if (!checkFunctions.isDuplicateServerGroup(clientId, ServerGroupID))
                 ClientList[clientId].addServerGroup(ServerGroupID, DateTime);
             }
@@ -205,7 +205,7 @@ module.exports = {
           }
 
           // Query client connects
-          else if (currentLine.indexOf(match.queryClientConnect) == beginOfParsingBlock) {
+          else if (currentLine.indexOf(match.queryClientConnect) === beginOfParsingBlock) {
             res = parsers.client.parseQueryClientConnect(currentLine);
 
             DateTime = this.parseDateTime(currentLine);
@@ -213,7 +213,7 @@ module.exports = {
 
             Client.fillArrayWithDummyClients(ClientList, clientId);
 
-            if (ClientList[clientId].clientId == -1)
+            if (ClientList[clientId].clientId === -1)
               ClientList[clientId].updateClientId(clientId);
 
             ClientList[clientId].addNickname(res.Nickname);
@@ -229,15 +229,15 @@ module.exports = {
 
           // Ban Rules
           // Currently only used for rules of 'direct' (= right click on client and "ban client") bans.
-          else if (currentLine.indexOf(match.banRule) == beginOfParsingBlock) {
-            if (currentLine.indexOf("' cluid='") != -1 && currentLine.indexOf("' ip='") == -1)
+          else if (currentLine.indexOf(match.banRule) === beginOfParsingBlock) {
+            if (currentLine.indexOf("' cluid='") !== -1 && currentLine.indexOf("' ip='") === -1)
               lastUIDBanRule = currentLine;
-            else if (currentLine.indexOf("' ip='") != -1 && currentLine.indexOf("' cluid='") == -1)
+            else if (currentLine.indexOf("' ip='") !== -1 && currentLine.indexOf("' cluid='") === -1)
               lastIPBanRule = currentLine;
           }
 
           // Complaints
-          else if (currentLine.indexOf(match.complaint) == beginOfParsingBlock) {
+          else if (currentLine.indexOf(match.complaint) === beginOfParsingBlock) {
             var res = parsers.complaint.parseComplaint(currentLine);
 
             DateTime = this.parseDateTime(currentLine);
@@ -248,31 +248,31 @@ module.exports = {
           }
 
           // Client Deletions
-          else if (currentLine.indexOf(match.deleteClient1) == beginOfParsingBlock && (currentLine.indexOf(match.deleteClient2) != -1)) {
+          else if (currentLine.indexOf(match.deleteClient1) === beginOfParsingBlock && (currentLine.indexOf(match.deleteClient2) !== -1)) {
             ClientList[parsers.client.parseClientDeletion(currentLine)].deleteClient();
           }
 
           // Servergroup creation, deletions, renaming and copying
-          else if (currentLine.indexOf(match.serverGroupEvent) == beginOfParsingBlock) {
+          else if (currentLine.indexOf(match.serverGroupEvent) === beginOfParsingBlock) {
             // 0 --> creation
             // 1 --> deleted
             // 2 --> renamed
             // 3 --> copied // just like 0
             var eventTypeS = -1;
 
-            if (currentLine.indexOf(match.serverGroupCreation) != -1) {
+            if (currentLine.indexOf(match.serverGroupCreation) !== -1) {
               eventTypeS = 0;
               res = parsers.serverGroup.parseServerGroupCreation(currentLine);
             }
-            else if (currentLine.indexOf(match.serverGroupDeletion) != -1) {
+            else if (currentLine.indexOf(match.serverGroupDeletion) !== -1) {
               eventTypeS = 1;
               res = parsers.serverGroup.parseServerGroupDeletion(currentLine);
             }
-            else if (currentLine.indexOf(match.serverGroupRenaming) != -1) {
+            else if (currentLine.indexOf(match.serverGroupRenaming) !== -1) {
               eventTypeS = 2;
               res = parsers.serverGroup.parseServerGroupRenaming(currentLine);
             }
-            else if (currentLine.indexOf(match.serverGroupCopying) != -1) {
+            else if (currentLine.indexOf(match.serverGroupCopying) !== -1) {
               eventTypeS = 3;
               res = parsers.serverGroup.parseServerGroupCopying(currentLine);
             }
@@ -281,7 +281,7 @@ module.exports = {
             ServerGroupID = res.ServerGroupID;
             var ServerGroupName = res.ServerGroupName;
 
-            if (eventTypeS == 1 || eventTypeS == 2) {
+            if (eventTypeS === 1 || eventTypeS === 2) {
               if (ServerGroup.getServerGroupByServerGroupId(ServerGroupList, ServerGroupID) === null)
                 ServerGroup.addServerGroup(ServerGroupList, ServerGroupID, "Unknown", ServerGroupName);
             }
@@ -307,7 +307,7 @@ module.exports = {
         // VirtualServerBase
         case 1:
           // Connects
-          if (currentLine.indexOf(match.connect) == beginOfParsingBlock) {
+          if (currentLine.indexOf(match.connect) === beginOfParsingBlock) {
             res = parsers.client.parseClientConnect(currentLine);
 
             DateTime = this.parseDateTime(currentLine);
@@ -315,7 +315,7 @@ module.exports = {
 
             Client.fillArrayWithDummyClients(ClientList, clientId);
 
-            if (ClientList[clientId].clientId == -1)
+            if (ClientList[clientId].clientId === -1)
               ClientList[clientId].updateClientId(clientId);
 
             ClientList[clientId].addNickname(res.Nickname);
@@ -333,12 +333,12 @@ module.exports = {
           }
 
           // Disconnects (including kicks and bans)
-          else if (currentLine.indexOf(match.disconnect) == beginOfParsingBlock) {
+          else if (currentLine.indexOf(match.disconnect) === beginOfParsingBlock) {
             var isBan = false,
               isKick = false;
 
-            if (currentLine.lastIndexOf(") reason 'reasonmsg") == -1) {
-              if (currentLine.lastIndexOf(" bantime=") == -1)
+            if (currentLine.lastIndexOf(") reason 'reasonmsg") === -1) {
+              if (currentLine.lastIndexOf(" bantime=") === -1)
                 isKick = true;
               else
                 isBan = true;
@@ -355,7 +355,7 @@ module.exports = {
               ClientList[clientId].updateClientId(clientId);
             }
 
-            if (ClientList[clientId].getNicknameCount() == 0 || ClientList[clientId].getNicknameByID(0) != Nickname)
+            if (ClientList[clientId].getNicknameCount() === 0 || ClientList[clientId].getNicknameByID(0) !== Nickname)
               ClientList[clientId].addNickname(Nickname);
 
             if (isLastLog)
@@ -380,26 +380,26 @@ module.exports = {
 
           // Todo: Optimize
           // Channel additions, edits or deletions.
-          else if (currentLine.indexOf(match.channel) == beginOfParsingBlock) {
+          else if (currentLine.indexOf(match.channel) === beginOfParsingBlock) {
             // 0 --> added
             // 1 --> edited
             // 2 --> edited undefined --> added
             // 3 --> deleted
             var eventTypeC = -1;
 
-            if (currentLine.indexOf(match.channelEdit) != -1) {
+            if (currentLine.indexOf(match.channelEdit) !== -1) {
               eventTypeC = 1;
               res = parsers.channel.parseChannelEdit(currentLine);
             }
-            else if (currentLine.indexOf(match.channelCreation) != -1) {
+            else if (currentLine.indexOf(match.channelCreation) !== -1) {
               eventTypeC = 0;
               res = parsers.channel.parseChannelCreation(currentLine);
             }
-            else if (currentLine.indexOf(match.subChannelCreation) != -1) {
+            else if (currentLine.indexOf(match.subChannelCreation) !== -1) {
               eventTypeC = 0;
               res = parsers.channel.parseSubChannelCreation(currentLine);
             }
-            else if (currentLine.indexOf(match.channelDeletion) != -1) {
+            else if (currentLine.indexOf(match.channelDeletion) !== -1) {
               eventTypeC = 3;
               res = parsers.channel.parseChannelDeletion(currentLine);
             }
@@ -410,7 +410,7 @@ module.exports = {
 
             var channelObject = Channel.getChannelByChannelId(ChannelList, channelID);
 
-            if (eventTypeC == 1 && channelObject === null)
+            if (eventTypeC === 1 && channelObject === null)
               eventTypeC = 2;
 
             switch (eventTypeC) {
@@ -438,7 +438,7 @@ module.exports = {
       // VirtualServerBase since version 3.0.12.0
       if (checkIfUpload) {
         // Uploads
-        if (currentLine.indexOf(match.upload) == beginOfParsingBlock) {
+        if (currentLine.indexOf(match.upload) === beginOfParsingBlock) {
           res = parsers.upload.parseUpload(currentLine);
           channelID = res.channelID;
 
@@ -450,7 +450,7 @@ module.exports = {
         }
 
         // Upload Deletions
-        else if (currentLine.indexOf(match.uploadDeletion) == beginOfParsingBlock) {
+        else if (currentLine.indexOf(match.uploadDeletion) === beginOfParsingBlock) {
           res = parsers.upload.parseUploadDeletion(currentLine);
 
           Upload.addDeletedBy(res.channelID, res.filename, res.deletedByID, res.deletedByNickname);

nodejs/ServerGroup.js
@@ -78,7 +78,7 @@ module.exports = {
    */
   getServerGroupByServerGroupId: function (array, serverGroupId) {
     for (var i = 0; i < array.length; i++) {
-      if (array[i].serverGroupId == serverGroupId)
+      if (array[i].serverGroupId === serverGroupId)
         return array[i];
     }
     return null;

nodejs/Upload.js
@@ -107,7 +107,7 @@ module.exports = {
     var UploadList = globalVariables.UploadList;
 
     for (var i = 0; i < UploadList.length; i++) {
-      if (UploadList[i].getChannelID() == channelID && UploadList[i].getFilename() == filename && !UploadList[i].isDeleted()) {
+      if (UploadList[i].getChannelID() === channelID && UploadList[i].getFilename() === filename && !UploadList[i].isDeleted()) {
         UploadList[i].deletedByID = deletedByID;
         UploadList[i].deletedByNickname = deletedByNickname;
         UploadList[i].deleted = true;

commit c6fbe9741bebf560a5fae9edb21494f3e93ac432

Updated install instructions regarding permission issues, npm install options and .htpaswd read access restrictions. Fixed comparison bug in TS3-LogDataViewer.js (caused by changes in 5d6781b8237b7b591e56c59cf09bfa8caf9c3009).

install instructions
@@ -21,6 +21,7 @@ You will require:
     - an user that is running the ts3-ldv app (e.g. ts3-ldv). This user must have read access to the logs of your
       teamspeak3 server (e.g. "/home/teamspeak/teamspeak3-server_linux_amd64/logs/") (could be achieved by adding the
       ts3-ldv user to a group that is running the teamspeak3 server and has access to the logs).
+      You will also have to open a shell as this user for the installation.
 
 Example creation of the ts3-ldv user as part of the teamspeak group (has access to the logs) (without a home directory):
     useradd -M --no-user-group -g teamspeak ts3-ldv
@@ -34,30 +35,48 @@ Installation:
 Create the ts3-ldv directory in /var/www/www.your-domain.com:
 (To prevent mix-ups with the ts3-ldv user and the directory it will be named ts3-ldv-dir, you can of course rename it)
     cd /var/www/www.your-domain.com
-    mkdir ts3-ldv-dir
+    sudo mkdir ts3-ldv-dir
 
 Make ts3-ldv the owner user and www-data the owner group of the ts3-ldv-dir (ts3-ldv needs to write into the directory,
 www-data needs to read):
-    chown ts3-ldv:www-data ts3-ldv-dir
+    sudo chown ts3-ldv:www-data ts3-ldv-dir
+
+From here on you should switch to the ts3-ldv user in order to not run into permission issues:
+    sudo su ts3-ldv
 
 Clone the TS3-LDV git repository into the folder:
     git clone https://github.com/Drumsticks1/TS3-LogDataViewer.git ts3-ldv-dir
 
+(Note if you didn't already switch to the ts3-ldv user: you should not install the npm dependencies as root because this
+may result in malicious code from rogue dependencies being executed as root!)
+
 Enter the ts3-ldv-dir directory and install the package dependencies:
     cd ts3-ldv-dir
-    npm install
+
+# Explanation of the npm install options:
+#     --no-cache:     installing the package without using the npm cache because otherwise npm would try to create a cache
+#                     directory in the ts3-ldv home directory that is not existing and would cause permission errors.
+#                         NOTE: Using this option creates a directory named 'false' in the install directory, you can
+#                               delete the directory or just ignore it.
+#     --production:   only install the production dependencies and ignore the devDependencies from the package.json
+    npm install --no-cache --production
 
 Copy the default configuration to the local folder and modify it according to your installation (see README.md for more
 information about the settings):
     cp conf/default-conf.json local/conf.json
     nano local/conf.json
 
+After that you can exit the ts3-ldv shell.
+
 Configure the web server so that only authorized clients have access to specific files (assuming you already have a
 working nginx configuration):
 Insert the following directives into the nginx configuration file of the domain (you will have to update the directory
 name and port if you modified them in your ts3-ldv configuration):
 
+# If you renamed ts3-ldv-dir locally, you will have to replace all occurrences of ts3-ldv-dir in the configuration
 location /ts3-ldv-dir {
+    # If you .htpasswd lies in a directory that can be accessed by the web server you should ensure that you prevent
+    # read access to .htpasswd files in your nginx configuration!
     auth_basic "Authentication required!";
     auth_basic_user_file /path/to/your/auth/file/.htpasswd;
 

js/TS3-LogDataViewer.js
@@ -46,7 +46,7 @@ function buildJSON(clearBuffer) {
   /* Requests are only sent if the time span between the last two requests is bigger than timeBetweenRequests and
    there is no request in progress. Invalid requests are only sent when the remote timeBetweenRequests is unknown
    or has increased since the last request */
-  if (!buildRequestInProgress && (lastBuildCallTime === null || timeBetweenBuilds === null || Date.now().valueOf() - lastBuildCallTime > timeBetweenBuilds)) {
+  if (!buildRequestInProgress && (lastBuildCallTime === undefined || timeBetweenBuilds === undefined || Date.now().valueOf() - lastBuildCallTime > timeBetweenBuilds)) {
     buildRequestInProgress = true;
     lastBuildCallTime = Date.now().valueOf();
 